ffc58777b5ef62afbaf1a6ad9262017ee63a6677
krentor471 <lib.ruina@gmail.com>
2025-11-05 13:47:01 +0300

    Что-то крутое)

--- src/Main.ts
+++ src/Main.ts
@@ -8,3 +8,13 @@ function arrive(time:number,timeLate:number) : number{
     return time+=timeLate
 }
 
+let formInput: HTMLInputElement = document.getElementById('input') as HTMLInputElement;
+let formOtput: HTMLInputElement = document.getElementById('output') as HTMLInputElement;
+
+formInput.addEventListener('input', setNumber(){
+    const input.Value: number = parseInt(formInput.value);
+    const FinalTime: number = arrive(InputValue);
+    formOutput.value = result.toString()
+});
+
+
