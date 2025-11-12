c20ce738ebff4ae13f1b55a7f420913163f3baf7
stl2802 <anmaks93@gmail.com>
2025-11-12 13:43:01 +0300

    Максимов Андрей Олегович

--- src/Main.ts
+++ src/Main.ts
@@ -1,3 +1,34 @@
 const InputElement = document.getElementById('formInput') as HTMLInputElement;
 const OutputElement = document.getElementById('formOutput') as HTMLInputElement;
 
+class scobki{
+    const strok:string
+    const variationSkobOpen = [
+        '(',
+        '[',
+        '{'
+    ];
+    const variationSkobClose = [
+        ')',
+        ']',
+        '}'
+    ];
+    constructor(strok: string, age: number) {
+        this.strok = strok;
+    }
+
+    public function check() {
+        let a = []
+        
+        for (const char of this.strok) {
+            if(char in this.variationSkobOpen){
+                a.push(char)
+            }else if(char in this.variationSkobClose){
+
+            }
+        }
+    }
+}
+
+
+[{]}
