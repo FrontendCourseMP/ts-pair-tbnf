3bdd2c97288a410eb837d65d5cacf9da7a0fc7d4
krentor471 <lib.ruina@gmail.com>
2025-11-12 13:20:55 +0300

    Колобанов А.Ю

--- src/Main.ts
+++ src/Main.ts
@@ -1,35 +1,3 @@
-const timeElement = document.getElementById('timeInput') as HTMLInputElement;
-const timeElement_2 = document.getElementById('timeInput_2') as HTMLInputElement;
-const lateElement = document.getElementById('timeLate') as HTMLInputElement;
-const form = document.getElementById("form") as HTMLFormElement;
+const InputElement = document.getElementById('formInput') as HTMLInputElement;
+const OutputElement = document.getElementById('formOutput') as HTMLInputElement;
 
-function arrive(time: number, timeLate: number): number {
-    return time + timeLate;
-}
-
-form.addEventListener('submit', (event: Event) => {
-    event.preventDefault();
-    
-    let time1: number;
-    let time2: number;
-    
-    try {
-        time1 = parseInt(timeElement.value, 10);
-        time2 = parseInt(timeElement_2.value, 10);
-    } catch {
-        lateElement.value = "Введите часы";
-        return;
-    }
-    
-    if (isNaN(time1) || isNaN(time2)) {
-        lateElement.value = "Введите числа";
-        return;
-    }
-    
-    let result: number = arrive(time1, time2);
-    while (result >= 24) {
-        result -= 24;
-    }
-    
-    lateElement.value = String(result);
-});
